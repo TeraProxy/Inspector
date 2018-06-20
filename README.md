16ca28ed08dfaa9787a1aef167f92ad90fd17009
TeraProxy <28067642+TeraProxy@users.noreply.github.com>
2018-06-20 02:34:53 +0200

    1.3.0
    * [+] Rewrote code to use Caali's "tera-game-state" module in order to reduce overhead
    * [+] Now supports auto-updating via Caali's tera-proxy
    * [+] Now prints applicant's item level to ingame chat
    * [+] Now prints applicant's clear count of currently relevant dungeons to ingame chat and console (thanks tera-love)
    * [~] Now using a json database instead of additional js files

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
 ##### :heavy_exclamation_mark: Status :heavy_exclamation_mark:
-Working on NA Godsfall patch with the latest https://github.com/meishuu/tera-data.  
-Please always keep your tera-data up-to-date.  
-Other regions will work if the opcodes are mapped but I personally only test modules on NA.  
+Should work on all regions as long as the opcodes are mapped but I personally only test modules on NA with Caali's tera-proxy: https://discord.gg/maqBmJV  
+
+##### :heavy_exclamation_mark: Installation for Caali's tera-proxy :heavy_exclamation_mark:
+1) Download Inspector: https://github.com/TeraProxy/Inspector/archive/master.zip
+2) Extract the contents of the zip file into "\tera-proxy\bin\node_modules\"
+3) Done! (the module will auto-update when a new version is released)
+
+##### :heavy_exclamation_mark: Installation for PinkiePie's tera-proxy :heavy_exclamation_mark:
+1) Update your tera-data: https://github.com/meishuu/tera-data
+2) Download Inspector: https://github.com/TeraProxy/Inspector/archive/master.zip
+3) Download tera-game-state: https://github.com/hackerman-caali/tera-game-state/archive/master.zip
+4) Extract the contents of both zip files into "\tera-proxy\bin\node_modules\"
+5) Done!
+6) Check back here once in a while for updates
 
 If you enjoy my work and wish to support future development, feel free to drop me a small donation: [![Donate](https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_100x26.png)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=A3KBZUCSEQ5RJ&lc=US&item_name=TeraProxy&curency_code=USD&no_note=1&no_shipping=1&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_SM%2egif%3aNonHosted)
-  
-# Inspector  
-A tera-proxy module that automatically inspects players when they apply for your group and logs details about them in your console.  
-Only inspects when you are out of combat so you don't get distracted while fighting.  
 
-![Screenshot](http://i.imgur.com/iEn1mtV.png)
+# Inspector  
+A tera-proxy module that automatically inspects players when they apply for your group and logs details about them in your console and ingame chat.  
+Only inspects when you are out of combat so you don't get distracted while fighting.  
   
-## Usage  
+![Screenshot](https://i.imgur.com/C3v27Zn.png)
+![Screenshot](https://i.imgur.com/oxTNFhO.png)
+
+## Usage
 The script is enabled by default and will automatically inspect players that apply for your group.  
   
 While in game, open a proxy chat session by typing "/proxy" or "/8" in chat and hitting the space bar.  
@@ -21,15 +33,21 @@ The following commands are supported:
 * inspect - enable/disable Inspector  
   
 ## Safety
-Whatever you send to the proxy chat in game is intercepted client-side. The chat is NOT sent to the server.  
-  
+Whatever you send to the proxy chat in game is intercepted client-side. The chat is NOT sent to the server.
+
 ## Changelog
+### 1.3.0
+* [+] Rewrote code to use Caali's "tera-game-state" module in order to reduce overhead
+* [+] Now supports auto-updating via Caali's tera-proxy
+* [+] Now prints applicant's item level to ingame chat
+* [+] Now prints applicant's clear count of currently relevant dungeons to ingame chat and console (thanks tera-love)
+* [~] Now using a json database instead of additional js files
 ### 1.2.1
-* [*] Updated hook versions for compatibility with the latest Tera-Proxy programs
+* [*] Updated hook versions for compatibility with the latest tera-proxy
 ### 1.2.0
 * [*] Some code cleanup
 * [*] Full conversion to Pinkie Pie's command module
 ### 1.1.0
 * [+] Added logging of a character's information to the console
 ### 1.0.0
-* [*] Initial Release
+* [~] Initial Release
--- index.js
+++ index.js
@@ -1,47 +1,54 @@
-// Version 1.2.1
+// Version 1.3.0
 
-const jobs = require('./jobs'),
-	races = require('./races'),
-	ItemStrings = require('item-strings'),
-	Command = require('command')
+'use strict'
+
+const ItemStrings = require('item-strings'),
+	Command = require('command'),
+	GameState = require('tera-game-state'),
+	races = require('./data.json')["races"],
+	jobs = require('./data.json')["jobs"],
+	dungeons = require('./data.json')["dungeons"]
 
 module.exports = function Inspector(dispatch) {
-	let cid = null,
-		enabled = true,
-		inCombat = false
-		
-	dispatch.hook('S_LOGIN', 10, event => {
-		cid = event.gameId
-		inCombat = false
-	})
+	const command = Command(dispatch),
+		game = GameState(dispatch)
 
-	dispatch.hook('S_OTHER_USER_APPLY_PARTY', 1, event => {
-		if (!enabled) return
+	let enabled = true,
+		name = ''
+
+	// ############# //
+	// ### Hooks ### //
+	// ############# //
+
+	dispatch.hook('S_OTHER_USER_APPLY_PARTY', 1, applying)
+
+	dispatch.hook('S_USER_PAPERDOLL_INFO', 4, inspect)
+
+	dispatch.hook('S_DUNGEON_CLEAR_COUNT_LIST', 1, clearCount)
+
+	// ################# //
+	// ### Functions ### //
+	// ################# //
+
+	function applying(event) {
+		if(!enabled) return
 		let name = event.name,
 			level = event.level,
 			job = event.class,
 			gender = event.gender,
 			race = event.race
-		if (!inCombat) {
+		if(!game.me.inCombat) {
 			//setTimeout( function inspect() {
 					dispatch.toServer('C_REQUEST_USER_PAPERDOLL_INFO', 1, { name: name })
 				//}, 2000)
 		}
 		console.log('[Inspector] ' + name + ' has applied to your group')
-	})
-	
-	dispatch.hook('S_USER_STATUS', 1, event => { 
-		if(event.target.equals(cid)) {
-			if(event.status == 1) {
-				inCombat = true
-			}
-			else inCombat = false
-		}
-	})
-	
-	dispatch.hook('S_USER_PAPERDOLL_INFO', 4, event => {
-		let name = event.name,
-			level = event.level,
+	}
+
+	function inspect(event) {
+		name = event.name
+
+		let	level = event.level,
 			race = Math.floor((event.model - 100) / 200 % 50), // 0 Human, 1 High Elf, 2 Aman, 3 Castanic, 4 Popori/Elin, 5 Baraka
 			gender = Math.floor(event.model / 100 % 2) + 1, // 1 female, 2 male
 			job = event.model % 100 - 1, // 0 Warrior, 1 Lancer, [...], 12 Valkyrie
@@ -57,7 +64,9 @@ module.exports = function Inspector(dispatch) {
 			weaponenchant, chestenchant, glovesenchant, bootsenchant,
 			weaponcrystal1, weaponcrystal2, weaponcrystal3, weaponcrystal4,
 			chestcrystal1, chestcrystal2, chestcrystal3, chestcrystal4
-			
+
+		command.message('[Inspector] ' + name + '\'s Item Level: ' + itemLevel + '/' + itemLevelInventory)
+
 		for(let item of event.items) {
 			switch(item.slot) {
 				case 1: // weapon
@@ -82,62 +91,65 @@ module.exports = function Inspector(dispatch) {
 					break;
 			}
 		}
-		console.log('[Inspector] ' + name + ' (Level ' + level + ' ' + getJob(job) + ' - ' + getGender(gender) + ' ' + getRace(race, gender) + ')')
-		console.log('            ' + guild + ' - Itemlevel: ' + itemLevel + '(' + itemLevelInventory + ')')
-		console.log('            ' + 'Weapon: ' + conv(weapon) + ' +' + weaponenchant)
-		console.log('            ' + '        ' + weaponcrystal1)
-		console.log('            ' + '        ' + weaponcrystal2)
-		console.log('            ' + '        ' + weaponcrystal3)
-		console.log('            ' + '        ' + weaponcrystal4)
-		console.log('            ' + 'Armor: ' + conv(chest) + ' +' + chestenchant)
-		console.log('            ' + '       ' + chestcrystal1)
-		console.log('            ' + '       ' + chestcrystal2)
-		console.log('            ' + '       ' + chestcrystal3)
-		console.log('            ' + '       ' + chestcrystal4)
-		console.log('            ' + 'Gloves: ' + conv(gloves) + ' +' + glovesenchant)
-		console.log('            ' + 'Boots: ' + conv(boots) + ' +' + bootsenchant)
-		console.log('            ' + 'Innerwear: ' + conv(innerwear))
-		console.log('            ' + 'Circlet: ' + conv(circlet))
-	})
-	
-	// ######################## //
-	// ### Helper Functions ### //
-	// ######################## //
-	
-	function get(obj, ...keys) {
-		if(obj === undefined) return
-
-		for(let key of keys)
-			if((obj = obj[key]) === undefined)
-				return
-
-		return obj
+		console.log('[Inspector] ' + name + ' (Level ' + level + ' ' + getJob(job) + ' - ' + getGender(gender) + ' ' + getRace(race, gender) + ')\n' +
+					'            ' + guild + ' - Itemlevel: ' + itemLevel + '(' + itemLevelInventory + ')\n' +
+					'            ' + 'Weapon: ' + conv(weapon) + ' +' + weaponenchant + '\n' +
+					'            ' + '        ' + weaponcrystal1 + '\n' +
+					'            ' + '        ' + weaponcrystal2 + '\n' +
+					'            ' + '        ' + weaponcrystal3 + '\n' +
+					'            ' + '        ' + weaponcrystal4 + '\n' +
+					'            ' + 'Armor: ' + conv(chest) + ' +' + chestenchant + '\n' +
+					'            ' + '       ' + chestcrystal1 + '\n' +
+					'            ' + '       ' + chestcrystal2 + '\n' +
+					'            ' + '       ' + chestcrystal3 + '\n' +
+					'            ' + '       ' + chestcrystal4 + '\n' +
+					'            ' + 'Gloves: ' + conv(gloves) + ' +' + glovesenchant + '\n' +
+					'            ' + 'Boots: ' + conv(boots) + ' +' + bootsenchant + '\n' +
+					'            ' + 'Innerwear: ' + conv(innerwear) + '\n' +
+					'            ' + 'Circlet: ' + conv(circlet) + '\n'
+		)
 	}
-	
+
+	function clearCount(event) {
+		if(game.me.playerId == event.pid) return // for some reason to game retrieves our own dungeon clears as well
+
+		command.message('\t\t' + name + '\'s dungeon clears:')
+		console.log('            ' + name + '\'s dungeon clears:')
+		for(let dungeon of event.dungeons) {
+			for(let mappeddungeon of dungeons) {
+				if(dungeon.id in mappeddungeon) {
+					let clearstring = mappeddungeon[dungeon.id] + '\t' + dungeon.clears + ' clears'
+					command.message('\t\t' + clearstring)
+					console.log('            ' + clearstring)
+				}
+			}
+		}
+		console.log('\n')
+	}
+
 	function getJob(job) {
-		return get(jobs, job, "name") || "Undefined"
+		return jobs[job] || "Undefined"
 	}
-	
+
 	function getRace(race, gender) {
-		if((race == 4) && (gender == 1)) return "Elin"
-		return get(races, race, "name") || "Undefined"
+		if(race == 4 && gender == 1) return "Elin"
+		return races[race] || "Undefined"
 	}
-	
+
 	function getGender(gender) {
 		if(gender == 1) return "female"
 		else if(gender == 2) return "male"
 		else return "Undefined"
 	}
-	
+
 	function conv(s) {
 		return ItemStrings(s) || "Undefined";
 	}
-  
-	// ################# //
-	// ### Chat Hook ### //
-	// ################# //
-	
-	const command = Command(dispatch)
+
+	// ################ //
+	// ### Commands ### //
+	// ################ //
+
 	command.add('inspect', () => {
 		enabled = !enabled
 		command.message('[Inspector] ' + (enabled ? '<font color="#56B4E9">enabled</font>' : '<font color="#E69F00">disabled</font>'))
