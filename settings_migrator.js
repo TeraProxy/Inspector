24c4c46560749014c7be9ca74a7abe650b5e9cc1
TeraProxy <28067642+TeraProxy@users.noreply.github.com>
2019-01-18 18:25:45 +0100

    Include VS and SS

--- settings_migrator.js
+++ settings_migrator.js
@@ -7,14 +7,17 @@ const DefaultSettings = {
 	"inspectDelay": 0,
 	"BS": true,
 	"GLSHM": true,
-	"GLSNM": true,
-	"MM": false,
 	"AAHM": true,
+	"GLSNM": false,
+	"MM": false,
+	"VSHM": true,
 	"DRCHM": true,
-	"HH20": true,
-	"AANM": false,
-	"DRCNM": true,
+	"HH20": false,
 	"RKHM": false,
+	"SSHM": false,
+	"DRCNM": false,
+	"AANM": false,
+	"VSNM": false,
 	"RRHM": false,
 	"RMHM": false,
 	"BPNM": false,
@@ -22,6 +25,7 @@ const DefaultSettings = {
 	"RKNM": false,
 	"RRNM": false,
 	"RMNM": false,
+	"SSNM": false,
 	"LKHM": false,
 	"TRHM": false,
 	"KDNM": false,
@@ -39,41 +43,41 @@ const DefaultSettings = {
 }
 
 module.exports = function MigrateSettings(from_ver, to_ver, settings) {
-    if (from_ver === undefined) {
-        // Migrate legacy config file
-        return Object.assign(Object.assign({}, DefaultSettings), settings);
-    } else if (from_ver === null) {
-        // No config file exists, use default settings
-        return DefaultSettings;
-    } else {
-        // Migrate from older version (using the new system) to latest one
-        if (from_ver + 1 < to_ver) {
-            // Recursively upgrade in one-version steps
-            settings = MigrateSettings(from_ver, from_ver + 1, settings);
-            return MigrateSettings(from_ver + 1, to_ver, settings);
-        }
-        
-        // If we reach this point it's guaranteed that from_ver === to_ver - 1, so we can implement
-        // a switch for each version step that upgrades to the next version. This enables us to
-        // upgrade from any version to the latest version without additional effort!
-        switch(to_ver)
-        {
+	if (from_ver === undefined) {
+		// Migrate legacy config file
+		return Object.assign(Object.assign({}, DefaultSettings), settings);
+	} else if (from_ver === null) {
+		// No config file exists, use default settings
+		return DefaultSettings;
+	} else {
+		// Migrate from older version (using the new system) to latest one
+		if (from_ver + 1 < to_ver) {
+			// Recursively upgrade in one-version steps
+			settings = MigrateSettings(from_ver, from_ver + 1, settings);
+			return MigrateSettings(from_ver + 1, to_ver, settings);
+		}
+
+		// If we reach this point it's guaranteed that from_ver === to_ver - 1, so we can implement
+		// a switch for each version step that upgrades to the next version. This enables us to
+		// upgrade from any version to the latest version without additional effort!
+		switch(to_ver)
+		{
 			// default is basically keeping the inspectDelay setting only
-            default:
+			default:
 				let oldsettings = settings
-				
+
 				settings = Object.assign(DefaultSettings, {});
-				
+
 				for(let option in oldsettings) {
 					if(settings[option]) {
 						settings[option] = oldsettings[option]
 					}
 				}
-				
+
 				if(from_ver < to_ver) console.log('[Inspector] Your settings have been updated to version ' + to_ver + '. You can edit the new config file after the next relog.')
 				break;
-        }
-        
-        return settings;
-    }
+		}
+
+		return settings;
+	}
 }
